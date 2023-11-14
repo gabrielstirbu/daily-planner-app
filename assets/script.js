ab9432e6060ee9220ea97e7d6abb873b7b16805e
GabrielCS22 <108716323+GabrielCS22@users.noreply.github.com>
2023-11-14 18:20:37 +0000

    Adding a fuction to color the event field cosidering the time

--- assets/script.js
+++ assets/script.js
@@ -6,3 +6,22 @@ function displayCurrentDay() {
 }
 // Call the function in order to be displayed on the page
 displayCurrentDay();
+
+// Function to color the event considering the time
+function colorCodeTimeblocks() {
+    var currentHour = dayjs().hour();
+
+    $(".time-block").each(function () {
+        var blockHour = parseInt($(this).attr("data-hour"));
+
+        if (blockHour < currentHour) {
+            $(this).find(".description").addClass("past");
+        } else if (blockHour === currentHour) {
+            $(this).find(".description").addClass("present");
+        } else {
+            $(this).find(".description").addClass("future");
+        }
+    });
+}
+// Call the function in order to be displayed on the page
+colorCodeTimeblocks();
